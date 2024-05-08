c23fbff7ccccdf2f78e16bb613d431e918abf299
shwan <tnghks023@naver.com>
2024-05-09 01:49:46 +0900

    baseURL modify

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:9192",
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "lake-side-hotel-back:8080",
 });
 
 export const getHeader = () => {
